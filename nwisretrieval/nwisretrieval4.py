08b4aea47e0848ae7b5107c755308b034cac9da1
O'Connell, Jonathan P <joconnell@usgs.gov>
2023-02-15 07:40:13 -0700

    working well

--- nwisretrieval/nwisretrieval4.py
+++ nwisretrieval/nwisretrieval4.py
@@ -3,35 +3,6 @@ import numpy as np
 import requests
 
 
-# class SubclassedDataFrame2(pd.DataFrame):
-#     _metadata = ["added_prop"]
-
-#     @property
-#     def _constructor(self):
-#         return SubclassedDataFrame2
-
-
-# df = SubclassedDataFrame2({"A": [1, 2, 3], "B": [4, 5, 6], "C": [7, 8, 9]})
-# df.added_property = "property"
-# df.added_property
-
-
-# class CustomFrame(pd.DataFrame):
-#     _metadata = ["_metadict", "start_date", " end_date"]
-
-#     @property
-#     def _constructor(self):
-#         return CustomFrame
-
-#     @property
-#     def start_date(self):
-#         return self._metadict.get("start_date")
-
-#     @property
-#     def end_date(self):
-#         return self._metadict.get("end_date")
-
-
 def get_nwis(
     STAID: str,
     start_date: str,
@@ -55,74 +26,101 @@ def get_nwis(
     dataframe["value"] = pd.to_numeric(dataframe["value"])
 
     metadata = {
-        "start": start_date,
-        "end": end_date,
-        "gap_flag": None,
-        "gap_tol": None,
+        "_STAID": STAID,
+        "_start_date": start_date,
+        "_end_date": end_date,
+        "_parame": param,
+        "_stat_code": stat_code,
+        "_service": service,
+        "_access_level": access,
+        "_url": url,
+        # "_gap_tol": gap_tol,
+        # "_gap_fill": gap_fill,
+        # "_resolve_masking": resolve_masking,
+        "_gap_flag": None,
+        "_site_name": rdata["value"]["timeSeries"][0]["sourceInfo"]["siteName"],
+        "_coords": (
+            rdata["value"]["timeSeries"][0]["sourceInfo"]["geoLocation"]["geogLocation"]["latitude"],
+            rdata["value"]["timeSeries"][0]["sourceInfo"]["geoLocation"]["geogLocation"]["longitude"],
+        ),
+        "_var_description": rdata["value"]["timeSeries"][0]["variable"]["variableDescription"],
     }
-    # Save metadata to dataframe
     dataframe = NWISFrame(dataframe)
-    # dataframe._metadata[0] = metadata
-    # dataframe.start_date = start_date
-    # dataframe.end_date = end_date
     dataframe._metadict = metadata
     return dataframe
 
 
-# @pd.api.extensions.register_dataframe_accessor("nwismeta")
-# class NWISMeta:
-#     def __init__(self, pandas_obj) -> None:
-#         self._obj = pandas_obj
-#         self._metadict: dict = {}
-
-
-# @pd.api.extensions.register_dataframe_accessor("nwisframe")
 class NWISFrame(pd.DataFrame):
-    # register custom properties in this list.  Allows for propogation of metadata after dataframe operations.
-    _metadata = ["_metadict", "start", " end", "gap_tol", "gap_flag"]
-
-    # def __init__(self, *args, **kwargs):
-    #     if "metadata" in kwargs:
-    #         self._metadict = kwargs["metadata"]
-    #         kwargs.pop("metadata")
-    #     super(NWISFrame, self).__init__(*args, **kwargs)
-    #     self.start_date = self._metadict["start_date"]
-    #     self.end_date = self._metadict["end_date"]
+    # register custom properties in this list.  Allows for propogation of metadata after dataframe manipulations e.g. slicing, asfreq, etc.
+    _metadata = ["_metadict"]
 
     @property
     def _constructor(self):
         return NWISFrame
 
     @property
+    def STAID(self):
+        return self._metadict.get("_STAID")
+
+    @property
     def start_date(self):
-        return str(self._metadict.get("start"))
-        # return str(self._metadata[0].get("start_date"))
+        return str(self._metadict.get("_start_date"))
 
     @property
     def end_date(self):
-        return str(self._metadict.get("end"))
+        return str(self._metadict.get("_end_date"))
 
-    # @property
-    # def gap_tol(self):
-    #     return str(self._metadict.get("gap_tol"))
+    @property
+    def param(self):
+        return self._metadict.get("_param")
+
+    @property
+    def stat_code(self):
+        return self._metadict.get("_stat_code")
+
+    @property
+    def service(self):
+        return self._metadict.get("_service")
+
+    @property
+    def url(self):
+        return self._metadict.get("_url")
+
+    @property
+    def site_name(self):
+        return self._metadict.get("_site_name")
+
+    @property
+    def coords(self):
+        return self._metadict.get("_coords")
+
+    @property
+    def var_description(self):
+        return self._metadict.get("_var_description")
+
+    @property
+    def gap_tolerance(self):
+        return str(self._metadict.get("_gap_tol"))
+
+    @property
+    def gaps(self):
+        return self._metadict.get("_gap_flag")
 
     def fill_gaps(
         self,
         interval: str | None = None,
     ):
-        # if interval is None:
-        #     interval = self.gap_tol
-        # reassignment of self._obj loses original _metadict
-        # metadata = self._metadict
-        self = self.asfreq(freq=interval)  # , NWISFrame(metadata=self._metadict)
+        if interval is None:
+            interval = self.gap_tolerance
+        self = self.asfreq(freq=interval)
         return self
 
     def check_gaps(
         self,
         interval: str,
     ) -> None:
-        # if interval is None:
-        #     interval = self.gap_tol
+        if interval is None:
+            interval = self.gap_tolerance
         idx = pd.date_range(self.start_date, self.start_date, freq=interval)
         if idx.difference(self.index).empty:
             self.gap_flag = False
@@ -130,7 +128,18 @@ class NWISFrame(pd.DataFrame):
 
 
 gap_data = get_nwis(STAID="12301933", start_date="2023-01-03", end_date="2023-01-04", param="63680")
+print(gap_data.STAID)
 print(gap_data.start_date)
+print(gap_data.end_date)
+print(gap_data.param)
+print(gap_data.stat_code)
+print(gap_data.url)
+print(gap_data.site_name)
+print(gap_data.coords)
+print(gap_data.var_description)
+print(gap_data.gap_tolerance)
+print(gap_data.gaps)
+
 gap_data = gap_data.fill_gaps("15min")
 gap_data.check_gaps("15min")
 pause = 2
