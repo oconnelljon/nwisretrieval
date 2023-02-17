5142db35d3598da6b287f490386da36061533e3b
O'Connell, Jonathan P <joconnell@usgs.gov>
2023-02-16 17:05:12 -0700

    looking good

--- nwisretrieval/nwisretrieval4.py
+++ nwisretrieval/nwisretrieval4.py
@@ -1,61 +1,63 @@
 import pandas as pd
 import numpy as np
 import requests
+import warnings
+from requests.models import Response
 
 
-def get_nwis(
-    STAID: str,
-    start_date: str,
-    end_date: str,
-    param: str,
-    stat_code: str = "32400",
-    service: str = "iv",
-    access: int = 0,
-):
-    service_urls = {
-        "dv": f"https://nwis.waterservices.usgs.gov/nwis/dv/?format=json&sites={STAID}&startDT={start_date}&endDT={end_date}&statCd={stat_code}&parameterCd={param}&siteStatus=all&access={access}",
-        "iv": f"https://nwis.waterservices.usgs.gov/nwis/iv/?format=json&sites={STAID}&parameterCd={param}&startDT={start_date}&endDT={end_date}&siteStatus=all&access={access}",
-    }
-    url = service_urls[service]
-
-    response = requests.request("GET", url)
-    rdata = response.json()
-    dataframe = pd.json_normalize(rdata, ["value", "timeSeries", "values", "value"])
-    dataframe["dateTime"] = pd.to_datetime(dataframe["dateTime"], infer_datetime_format=True)
-    dataframe.set_index("dateTime", inplace=True)
-    dataframe["value"] = pd.to_numeric(dataframe["value"])
-
-    metadata = {
-        "_STAID": STAID,
-        "_start_date": start_date,
-        "_end_date": end_date,
-        "_parame": param,
-        "_stat_code": stat_code,
-        "_service": service,
-        "_access_level": access,
-        "_url": url,
-        # "_gap_tol": gap_tol,
-        # "_gap_fill": gap_fill,
-        # "_resolve_masking": resolve_masking,
-        "_gap_flag": None,
-        "_site_name": rdata["value"]["timeSeries"][0]["sourceInfo"]["siteName"],
-        "_coords": (
-            rdata["value"]["timeSeries"][0]["sourceInfo"]["geoLocation"]["geogLocation"]["latitude"],
-            rdata["value"]["timeSeries"][0]["sourceInfo"]["geoLocation"]["geogLocation"]["longitude"],
-        ),
-        "_var_description": rdata["value"]["timeSeries"][0]["variable"]["variableDescription"],
-    }
-    dataframe = NWISFrame(dataframe)
-    dataframe._metadict = metadata
-    return dataframe
+def query_url(
+    url: str,
+) -> Response:
+    response = requests.get(url)
+    if response.status_code != 200:
+        print(f"Critical error!  No data found at: {url}\n Reason: {response.reason}")
+        raise SystemExit
+    return response
 
 
 class NWISFrame(pd.DataFrame):
-    # register custom properties in this list.  Allows for propogation of metadata after dataframe manipulations e.g. slicing, asfreq, etc.
+    """Inherits from pandas DataFrame to extend properties and methods specific to NWIS time-series data.
+
+    Parameters
+    ----------
+    pd.DataFrame : pd.DataFrame
+        pd.DataFrame to inherit from.
+
+    Returns
+    -------
+    NWISFrame
+        Extended pd.DataFrame.  Acts like a normal DataFrame, but with NWIS specific properties and methods.
+
+    Notes
+    -----
+        Generally you don't want to inherit from pd.DataFrame.  Pandas offers an API for extending
+    DataFrame but there are complications with propogating user defined properties.
+    When an operation on the DataFrame returns a new DataFrame, user defined properties
+    do not propogate.  Inheriting from DataFrame allows for the use of the
+    internal "private" DataFrame property "_metadata" which contains a list of user defined variables
+    that will be propogated during DataFrame operations when a new DataFrame is returned.
+
+    In the case of this custom class, _metadata contains one user defined variable "_metadict"
+    which is a dictionary populated with metadata from the get_nwis function.
+    The properties query the _metadict dictionary of the DataFrame to return metadata values such as
+    the URL, start date, variable description, etc.
+
+    For more information on extending pandas:
+        https://pandas.pydata.org/docs/dev/development/extending.html#
+
+    @properties cannot be set by assignment e.g. STAID = "12301933" or self.STAID = "12301933"
+    However, they are accessed like a class variable e.g. my_dataframe.STAID returns "12301933"
+    """
+
+    # register custom dataframe properties in this list.
+    # Allows for propogation of metadata after dataframe manipulations e.g. slicing, asfreq, copy(), etc.
+    # _metadict is created and assigned to the dataframe from get_nwis.
     _metadata = ["_metadict"]
 
     @property
     def _constructor(self):
+        # Override of pd.DataFrame _constructor property.
+        # Allows for the retention of subclasses through data manipulations.
         return NWISFrame
 
     @property
@@ -100,46 +102,225 @@ class NWISFrame(pd.DataFrame):
 
     @property
     def gap_tolerance(self):
-        return str(self._metadict.get("_gap_tol"))
+        return str(self._metadict.get("_gap_tolerance"))
 
     @property
-    def gaps(self):
-        return self._metadict.get("_gap_flag")
+    def gap_flag(self):
+        return self.check_gaps(self.gap_tolerance)
 
-    def fill_gaps(
-        self,
-        interval: str | None = None,
-    ):
-        if interval is None:
-            interval = self.gap_tolerance
-        self = self.asfreq(freq=interval)
-        return self
+    @property
+    def mask_flag(self):
+        return self._metadict.get("_mask_flag")
+
+    @property
+    def approval(self):
+        return self.check_approval()
+
+    @property
+    def qualifier(self):
+        return self.check_quals()
+
+    def check_quals(self) -> str:
+        """Checks if qualifiers are applied to data.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        str
+            Returns "Ice" if ANY record has an ice qualifier.
+
+        Notes
+        -----
+        Currently only checking for Ice qualifiers.  May need to add more for equipment malfunctions, etc.
+        """
+        unique_quals = list(pd.unique(self["qualifiers"].apply(frozenset)))
+        for qual in unique_quals:
+            if "Ice" in qual or "i" in qual:
+                return "Ice"
+        return "None"
+
+    def resolve_masks(self) -> None:
+        """Convert any values from -999999 to NaN values.
+        Set True to resolve NWIS masking of Ice qualified data to NaN values.
+        NWIS serves public data with an Ice qualifier as -999999 masked values.
+        Setting access to 1 "coop" or 2 "internal" returns actual values.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
+
+        self["value"] = self["value"].where(self["value"] != -999999.0, np.NaN)
+        self._metadict["_mask_flag"] = True
+        return None
 
     def check_gaps(
         self,
-        interval: str,
-    ) -> None:
-        if interval is None:
-            interval = self.gap_tolerance
-        idx = pd.date_range(self.start_date, self.start_date, freq=interval)
-        if idx.difference(self.index).empty:
-            self.gap_flag = False
-            return None
+        gap_tol: str | None = None,
+    ) -> bool:
+        # If no gap_tol is specified but _metadict contains a gap_tolerance, then fall back on the gap_tolerance value.
+        if gap_tol is None and self.gap_tolerance is not None:
+            gap_tol = self.gap_tolerance
+
+        # Override _gap_tol in _metadict with specified gap_tol
+        self._metadict["_gap_tol"] = gap_tol
+        try:
+            idx = pd.date_range(self.start_date, self.end_date, freq=gap_tol)
+
+            # If index differences are empty, then there must be no gaps.  Set and return gap_flag
+            if idx.difference(self.index).empty:
+                self._metadict["_gap_flag"] = False
+                return False
+
+            # Gaps found in the index
+            print(f"Gaps detected at: {self.STAID} with a tolerance of {gap_tol}")
+            self._metadict["_gap_flag"] = True
+            return True
+
+        # if invalid gap_tol, issue warning and return "unknown"
+        except ValueError as e:
+            self._metadict["_gap_tol"] = "unknown"
+            warnings.warn(f"Warning: No gap tolerance specified for {self.STAID}.")
+            return self._metadict["_gap_tol"]
+
+    def check_approval(self) -> str:
+        """Checks approval level of data.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        str
+            Return "Provisional" or "Approved".
+            If ANY records are provisional, set to "Provisional" level.
+
+        Notes
+        -----
+        """
+        unique_quals = list(pd.unique(self["qualifiers"].apply(frozenset)))
+        approval_level = next(
+            ("Provisional" for approval in unique_quals if "P" in approval),
+            "Approved",
+        )
+        self._metadict["_approval"] = approval_level
+        return approval_level
+
+    def fill_gaps(
+        self,
+        gap_tol: str | None = None,
+    ):
+        # If no gap_tol is specified but _metadict contains a gap_tolerance, then fall back on the gap_tolerance value.
+        if gap_tol is None and self.gap_tolerance is not None:
+            gap_tol = self.gap_tolerance
+
+        # Override _gap_tol in _metadict with specified gap_tol
+        self._metadict["_gap_tol"] = gap_tol
+        try:
+            self = self.asfreq(freq=gap_tol)
+            self._metadict["_gap_flag"] = False
+            return self
+
+        # if invalid gap_tol, issue warning and return "unknown"
+        except ValueError as e:
+            self._metadict["_gap_tol"] = "unknown"
+            warnings.warn(f"Warning: No gap tolerance specified for {self.STAID}.")
+            return self._metadict["_gap_tol"]
 
 
-gap_data = get_nwis(STAID="12301933", start_date="2023-01-03", end_date="2023-01-04", param="63680")
-print(gap_data.STAID)
-print(gap_data.start_date)
-print(gap_data.end_date)
-print(gap_data.param)
-print(gap_data.stat_code)
-print(gap_data.url)
-print(gap_data.site_name)
-print(gap_data.coords)
-print(gap_data.var_description)
-print(gap_data.gap_tolerance)
-print(gap_data.gaps)
+def get_nwis(
+    STAID: str,
+    start_date: str,
+    end_date: str,
+    param: str,
+    stat_code: str = "32400",
+    service: str = "iv",
+    access: int = 0,
+    gap_tol: str | None = None,
+    gap_fill: bool = False,
+    resolve_masking: bool = False,
+):
+    service_urls = {
+        "dv": f"https://nwis.waterservices.usgs.gov/nwis/dv/?format=json&sites={STAID}&startDT={start_date}&endDT={end_date}&statCd={stat_code}&parameterCd={param}&siteStatus=all&access={access}",
+        "iv": f"https://nwis.waterservices.usgs.gov/nwis/iv/?format=json&sites={STAID}&parameterCd={param}&startDT={start_date}&endDT={end_date}&siteStatus=all&access={access}",
+    }
+    url = service_urls[service]
 
-gap_data = gap_data.fill_gaps("15min")
-gap_data.check_gaps("15min")
-pause = 2
+    response = query_url(url)
+    rdata = response.json()
+    dataframe = pd.json_normalize(rdata, ["value", "timeSeries", "values", "value"])
+    dataframe["dateTime"] = pd.to_datetime(dataframe["dateTime"].array, infer_datetime_format=True)
+    dataframe.set_index("dateTime", inplace=True)
+    dataframe = dataframe.tz_localize(None)
+    dataframe["value"] = pd.to_numeric(dataframe["value"])
+
+    metadata = {
+        "_STAID": STAID,
+        "_start_date": start_date,
+        "_end_date": end_date,
+        "_parame": param,
+        "_stat_code": stat_code,
+        "_service": service,
+        "_access_level": access,
+        "_url": url,
+        "_gap_tolerance": gap_tol,
+        "_gap_fill": gap_fill,
+        "_resolve_masking": resolve_masking,
+        "_gap_flag": "unknown",
+        "_approval": "Provisional",
+        "_mask_flag": "unknown",
+        "_site_name": rdata["value"]["timeSeries"][0]["sourceInfo"]["siteName"],
+        "_coords": (
+            rdata["value"]["timeSeries"][0]["sourceInfo"]["geoLocation"]["geogLocation"]["latitude"],
+            rdata["value"]["timeSeries"][0]["sourceInfo"]["geoLocation"]["geogLocation"]["longitude"],
+        ),
+        "_var_description": rdata["value"]["timeSeries"][0]["variable"]["variableDescription"],
+    }
+
+    # Wrap pandas dataframe with custom NWISFrame class and assign metadata to _metadict.
+    dataframe = NWISFrame(dataframe)
+    dataframe._metadict = metadata
+
+    if gap_fill and bool(gap_tol):
+        dataframe = dataframe.fill_gaps(gap_tol)
+    if resolve_masking:
+        dataframe.resolve_masks()
+
+    return dataframe
+
+
+if __name__ == "__main__":
+
+    # gap_data = get_nwis(STAID="12301933", start_date="2023-01-03", end_date="2023-01-04", param="63680")
+    data_ice = get_nwis(STAID="12301250", start_date="2023-01-02", end_date="2023-01-03", param="00060", resolve_masking=False)
+    # print("STAID: ", gap_data.STAID)
+    # print("Start date: ", gap_data.start_date)
+    # print("End date: ", gap_data.end_date)
+    # print("Parameter: ", gap_data.param)
+    # print("Stat code: ", gap_data.stat_code)
+    # print("URL: ", gap_data.url)
+    # print("Site name: ", gap_data.site_name)
+    # print("Coordinates: ", gap_data.coords)
+    # print("Variable description: ", gap_data.var_description)
+    # print("Gap tolerance: ", gap_data.gap_tolerance)
+    # print("Data gaps: ", gap_data.gaps)
+
+    # gap_data = gap_data.fill_gaps("15min")
+    # print("Check data gaps")
+    # gap_data.check_gaps("15min")
+    # print("Data gaps: ", gap_data.gaps)
+    # print("filling data gaps")
+    # gap_data = gap_data.fill_gaps("15min")
+    # print("Check data gaps")
+    # gap_data.check_gaps("15min")
+    # print("Data gaps: ", gap_data.gaps)
+    # print("Approval level: ", gap_data.approval)
+    pause = 2
